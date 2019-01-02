1d8ef811f9e343094f7cc38b63c80cd9d27e4d56
julia-as <julia.e.as@gmail.com>
2019-01-02 14:01:21 +0100

    minor changes

--- src/laerm/client/app/LaermClient.java
+++ src/laerm/client/app/LaermClient.java
@@ -10,9 +10,18 @@ public class LaermClient {
 	public static void main(String[] args) {
 
 		try {
-			SensorModelImpl sensor = new SensorModelImpl();
-			Naming.lookup("rmi://localhost:1099/sensors/");
-			System.out.println(sensor.getSensor());
+			SensorModelImpl sensor0 = new SensorModelImpl();
+			Naming.lookup("rmi://localhost:1099/sensors/0");
+			System.out.println(sensor0.getSensor());
+			SensorModelImpl sensor1 = new SensorModelImpl();
+			Naming.lookup("rmi://localhost:1099/sensors/1");
+			System.out.println(sensor1.getSensor());
+			SensorModelImpl sensor2 = new SensorModelImpl();
+			Naming.lookup("rmi://localhost:1099/sensors/2");
+			System.out.println(sensor2.getSensor());
+			SensorModelImpl sensor3 = new SensorModelImpl();
+			Naming.lookup("rmi://localhost:1099/sensors/3");
+			System.out.println(sensor3.getSensor());
 		} 
 		catch (Exception e) { 
 			e.printStackTrace();
