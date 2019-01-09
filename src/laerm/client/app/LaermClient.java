76a8f1be5053f81a006140199cfc23bb5f111a21
julia-as <julia.e.as@gmail.com>
2019-01-09 12:39:27 +0100

    latest changes (observer)

--- src/laerm/client/app/LaermClient.java
+++ src/laerm/client/app/LaermClient.java
@@ -1,27 +1,43 @@
 package laerm.client.app;
 
 import java.rmi.Naming;
+import java.util.Observable;
+import java.util.Observer;
 
-import de.htw.vt.SensorModelImpl;
+import de.htw.vt.SensorImpl;
 
 
-public class LaermClient {
+public class LaermClient implements Observer {
+
+	LaermClient client;
+
+	public LaermClient() {}
 
 	public static void main(String[] args) {
 
+		LaermClient client = new LaermClient();
+
 		try {
-			SensorModelImpl sensor0 = new SensorModelImpl();
+			SensorImpl sensor0 = new SensorImpl();
 			Naming.lookup("rmi://localhost:1099/sensors/0");
+			sensor0.register(client);
 			System.out.println(sensor0.getSensor());
-			SensorModelImpl sensor1 = new SensorModelImpl();
+
+			SensorImpl sensor1 = new SensorImpl();
 			Naming.lookup("rmi://localhost:1099/sensors/1");
+			sensor0.register(client);
 			System.out.println(sensor1.getSensor());
-			SensorModelImpl sensor2 = new SensorModelImpl();
+
+			SensorImpl sensor2 = new SensorImpl();
 			Naming.lookup("rmi://localhost:1099/sensors/2");
+			sensor0.register(client);
 			System.out.println(sensor2.getSensor());
-			SensorModelImpl sensor3 = new SensorModelImpl();
+
+			SensorImpl sensor3 = new SensorImpl();
 			Naming.lookup("rmi://localhost:1099/sensors/3");
+			sensor0.register(client);
 			System.out.println(sensor3.getSensor());
+
 		} 
 		catch (Exception e) { 
 			e.printStackTrace();
@@ -29,4 +45,11 @@ public class LaermClient {
 		
 	}
 
+	@Override
+	public void update(Observable o, Object arg) {
+
+	}
+
+	//attach with subject to observe
+	public void register(SensorImpl sensor){};
 }
