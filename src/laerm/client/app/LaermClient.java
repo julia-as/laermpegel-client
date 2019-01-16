8aed611a0f0ebf3a179c038e3d02787cab51cf14
julia-as <julia.e.as@gmail.com>
2019-01-16 17:02:09 +0100

    seems to work alright

--- src/laerm/client/app/LaermClient.java
+++ src/laerm/client/app/LaermClient.java
@@ -3,12 +3,10 @@ package laerm.client.app;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
-import java.util.Observable;
-import java.util.Observer;
 
 import de.htw.vt.Sensor;
-import de.htw.vt.SensorImpl;
 import de.htw.vt.SensorObserver;
+import org.htw.fiw.FrontendServer;
 
 
 public class LaermClient extends UnicastRemoteObject implements SensorObserver {
@@ -18,6 +16,9 @@ public class LaermClient extends UnicastRemoteObject implements SensorObserver {
 	static Sensor sensor1;
 	static Sensor sensor2;
 	static Sensor sensor3;
+	static FrontendServer frontend;
+
+
 
 	public LaermClient() throws RemoteException {}
 
@@ -27,23 +28,26 @@ public class LaermClient extends UnicastRemoteObject implements SensorObserver {
 			LaermClient client = new LaermClient();
 			sensor0 = (Sensor)Naming.lookup("rmi://localhost:1099/sensors/0");
 			sensor0.register(client);
-			System.out.println("client(this): " + client.toString());
 			System.out.println(sensor0.writeToConsole());
 
-//			sensor1 = new SensorImpl();
-//			Naming.lookup("rmi://localhost:1099/sensors/1");
+			frontend = (FrontendServer)Naming.lookup("rmi://localhost:9876/FrontendServer");
+			Double value = Double.valueOf(sensor0.getValue());
+			frontend.receiveSensor(sensor0.getX(), sensor0.getY());
+			frontend.receiveValue(value);
+
+
+
+//			sensor1 = (Sensor)Naming.lookup("rmi://localhost:1099/sensors/1");
 //			sensor0.register(client);
-//			System.out.println(sensor1.toString());
+//			System.out.println(sensor1.writeToConsole());
 //
-//			sensor2 = new SensorImpl();
-//			Naming.lookup("rmi://localhost:1099/sensors/2");
+//			sensor2 = (Sensor)Naming.lookup("rmi://localhost:1099/sensors/2");
 //			sensor0.register(client);
-//			System.out.println(sensor2.toString());
+//			System.out.println(sensor2.writeToConsole());
 //
-//			sensor3 = new SensorImpl();
-//			Naming.lookup("rmi://localhost:1099/sensors/3");
+//			sensor3 = (Sensor)Naming.lookup("rmi://localhost:1099/sensors/3");
 //			sensor0.register(client);
-//			System.out.println(sensor3.toString());
+//			System.out.println(sensor3.writeToConsole());
 
 		} 
 		catch (Exception e) { 
@@ -51,8 +55,11 @@ public class LaermClient extends UnicastRemoteObject implements SensorObserver {
 		}
 	}
 
-	public void update(int value) throws RemoteException {
+	public void update(Sensor sensor, int value) throws RemoteException {
 		this.sensor0.setValue(value);
 		System.out.println(this.sensor0.writeToConsole());
+		Double dValue = new Double(value);
+		frontend.receiveValue(dValue);
+
 	}
 }
