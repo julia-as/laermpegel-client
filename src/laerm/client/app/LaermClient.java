458effd6341ab03d161fe16cc5fd2d082e6dddc2
julia-as <julia.e.as@gmail.com>
2019-01-16 13:14:01 +0100

    added observer pattern

--- src/laerm/client/app/LaermClient.java
+++ src/laerm/client/app/LaermClient.java
@@ -1,60 +1,58 @@
 package laerm.client.app;
 
 import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.Observable;
 import java.util.Observer;
 
 import de.htw.vt.Sensor;
 import de.htw.vt.SensorImpl;
+import de.htw.vt.SensorObserver;
 
 
-public class LaermClient implements Observer{
+public class LaermClient extends UnicastRemoteObject implements SensorObserver {
 
 	LaermClient client;
+	static Sensor sensor0;
+	static Sensor sensor1;
+	static Sensor sensor2;
+	static Sensor sensor3;
 
-	public LaermClient() {}
+	public LaermClient() throws RemoteException {}
 
-	public static void main(String[] args) {
-
-		LaermClient client = new LaermClient();
+	public static void main(String[] args)  {
 
 		try {
-			Sensor sensor0 = new SensorImpl();
-			Naming.lookup("rmi://localhost:1099/sensors/0");
+			LaermClient client = new LaermClient();
+			sensor0 = (Sensor)Naming.lookup("rmi://localhost:1099/sensors/0");
 			sensor0.register(client);
 			System.out.println("client(this): " + client.toString());
 			System.out.println(sensor0.toString());
 
-			SensorImpl sensor1 = new SensorImpl();
-			Naming.lookup("rmi://localhost:1099/sensors/1");
-			sensor0.register(client);
-			System.out.println(sensor1.toString());
-
-			SensorImpl sensor2 = new SensorImpl();
-			Naming.lookup("rmi://localhost:1099/sensors/2");
-			sensor0.register(client);
-			System.out.println(sensor2.toString());
-
-			SensorImpl sensor3 = new SensorImpl();
-			Naming.lookup("rmi://localhost:1099/sensors/3");
-			sensor0.register(client);
-			System.out.println(sensor3.toString());
+//			sensor1 = new SensorImpl();
+//			Naming.lookup("rmi://localhost:1099/sensors/1");
+//			sensor0.register(client);
+//			System.out.println(sensor1.toString());
+//
+//			sensor2 = new SensorImpl();
+//			Naming.lookup("rmi://localhost:1099/sensors/2");
+//			sensor0.register(client);
+//			System.out.println(sensor2.toString());
+//
+//			sensor3 = new SensorImpl();
+//			Naming.lookup("rmi://localhost:1099/sensors/3");
+//			sensor0.register(client);
+//			System.out.println(sensor3.toString());
 
 		} 
 		catch (Exception e) { 
 			e.printStackTrace();
 		}
-		
 	}
 
-	@Override
-	public void update(Observable o, Object obj){};
-
-	public void update(Sensor sensor, int value) {
-		sensor.setValue(value);
+	public void update(int value) throws RemoteException {
+		this.sensor0.setValue(value);
+		System.out.println(this.sensor0.toString());
 	}
-
-
-	//attach with subject to observe
-	public void register(SensorImpl sensor){};
 }
